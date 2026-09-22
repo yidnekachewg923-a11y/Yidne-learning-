b2803ce7fe1c9b4ae500e02057d3b60b67bd37e7
yidnekachewg923-a11y <yidnekachewg923@gmail.com>
2026-09-22 22:14:47 +0300

    Add files via upload

---
+++ grade10sub.js
@@ -0,0 +1,13 @@
+function grade10(){
+  document.body.innerHTML=`
+   <div id="biou">
+          <h1>Grade 10</h1>
+          <p>Before start learning choose the The subject</p>
+          <button onclick="P10()">Physics</button>
+          <button onclick="B10()">Biology</button>
+          <button onclick="C10()">Chemistry</button>
+          <button onclick="M10()">Mathematics</button>
+          <button onclick="E10()">English</button>
+      </div>
+  `;
+}
