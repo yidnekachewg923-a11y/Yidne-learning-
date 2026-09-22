90f6c81dea542da12961e7abf71350e35e7f9fd8
yidnekachewg923-a11y <yidnekachewg923@gmail.com>
2026-09-22 14:51:29 +0300

    Create initial Index.html for GitHub Pages

--- biology.js
+++ biology.js
@@ -3,8 +3,8 @@ function bio9(){
       <div id="biou">
           <h1>Welcome to biology</h1>
           <p>Before start learning please choose the unit</p>
-          <button onclick="bunit1()">Unit One</button>
-          <button onclick="Bunit2()">Unit Two</button>
+          <button onclick="Bunit1()">Unit One</button>
+          <button onclick="bunit2()">Unit Two</button>
           <button onclick="Bunit3()">Unit Three</button>
           <button onclick="Bunit4()">Unit Four</button>
           <button onclick="Bunit5()">Unit Five</button>
