bb536d3e4cb7af4dfcef27bb89d4054be25fab29
yidnekachewg923-a11y <yidnekachewg923@gmail.com>
2026-09-22 21:40:42 +0300

    Add files via upload

---
+++ grade10.js
@@ -0,0 +1,14 @@
+function grade10(){
+  document.body.innerHTML=`
+   <div id="biou">
+          <h1>Welcome to Physics</h1>
+          <p>Before start learning please choose the unit</p>
+          <button onclick="Punit1()">Unit One</button>
+          <button onclick="Punit2()">Unit Two</button>
+          <button onclick="Punit3()">Unit Three</button>
+          <button onclick="Punit4()">Unit Four</button>
+          <button onclick="Punit5()">Unit Five</button>
+          <button onclick="Punit6()">Unit Six</button>
+      </div>
+  `;
+}
